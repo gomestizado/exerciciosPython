36c4cede7536a5f3e746baafc8119d6015f2fdf0
gomestizado <matheus_mgc@yahoo.com.br>
2023-07-28 15:04:21 -0300

    corrige exercicios

--- ex016.py
+++
@@ -1,7 +0,0 @@
-
-from math import trunc
-
-valor = float(input('Digite um valor com casas decimais: '))
-
-print('O número {} tem a parte inteira {}'.format(valor, trunc(valor)))
-'''trunc(valor) || int(valor)'''
