175684b0cb059162e56f1505ebabcc5e0e4c1501
gomestizado <matheus_mgc@yahoo.com.br>
2023-07-28 14:58:09 -0300

    adiciona exercicios

---
+++ ex016.py
@@ -0,0 +1,7 @@
+
+from math import trunc
+
+valor = float(input('Digite um valor com casas decimais: '))
+
+print('O número {} tem a parte inteira {}'.format(valor, trunc(valor)))
+'''trunc(valor) || int(valor)'''
---
+++ ex019.py
@@ -0,0 +1,10 @@
+from random import choice
+
+a1 = input('Digite o primeiro nome: ')
+a2 = input('Digite o segundo nome: ')
+a3 = input('Digite o terceiro nome: ')
+a4 = input('Digite o quarto nome: ')
+
+alunos = [a1 , a2 , a3 , a4]
+
+print('{} é quem irá apagar o quadro dessa vez!'.format(choice(alunos)))
